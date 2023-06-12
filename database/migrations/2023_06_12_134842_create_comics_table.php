3e3cbb61c95f9c66c478aea4004a4173534566e4
Sara Gisondi <sara.gisondi@outlook.it>
2023-06-12 18:07:14 +0200

    add migration

--- database/migrations/2023_06_12_134842_create_comics_table.php
+++ database/migrations/2023_06_12_134842_create_comics_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title',100);
+            $table->text('description');
+            $table->string('thumb');
+            $table->decimal('price', $precision = 4, $scale = 2);
+            $table->string('series',50);
+            $table->dateTime('sale_date');
+            $table->string('type',30);
+            $table->string('artist');
+            $table->string('writers');
             $table->timestamps();
         });
     }
