07acfcf2f1d49ca2061a18aa59a6b26c33e35171
Sara Gisondi <sara.gisondi@outlook.it>
2023-06-12 15:59:12 +0200

    modify route & add comicController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout.main')
 
-        <title>DC-Comics</title>
-
-        {{-- Includiamo gli assets con la direttiva @vite --}}
-        @vite('resources/js/app.js')
-    </head>
-    <body>
-
-      @include('partials.header')
-        <main>
-            @yield('content')
-        </main>
-
-      @include('partials.footer')
-
-    </body>
-</html>
+@section('content')
+<h1>main</h1>
+@endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,5 +1,23 @@
-@extends('layout.main')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<h1>main</h1>
-@endsection
+        <title>DC-Comics</title>
+
+        {{-- Includiamo gli assets con la direttiva @vite --}}
+        @vite('resources/js/app.js')
+    </head>
+    <body>
+
+      @include('partials.header')
+
+        <main>
+            @yield('content')
+        </main>
+
+      @include('partials.footer')
+
+    </body>
+</html>
