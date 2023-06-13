2fe2ca8656f6954e8b71625acac972607c47652b
Sara Gisondi <sara.gisondi@outlook.it>
2023-06-13 15:39:30 +0200

    modify show.blade

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,27 +2,40 @@
 
 @section('content')
 
-<div class="container d-flex">
-  <div class="row">
-    @foreach ($comics as $comic)
-      <div class="col my-4" >
-          <div class="card px-4 h-100" style="width: 18rem;">
-            <div class="container-img  h-100 w-100">
-              <img src="{{$comic->thumb}}" class="card-img-top  alt="...">
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">{{$comic->title}}</h5>
-              <p class="card-text">{{$comic->price}}</p>
-              <p>{{$comic->type}}</p>
-              <p>{{$comic->sale_date}}</p>
-              <p>{{$comic->series}}</p>
-            </div>
-            <a href="{{route('comic.show', $comic)}}" class="btn btn-primary my-4">DESCRIZIONE</a>
-          </div>
-        </div>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Titolo</th>
+        <th scope="col">Prezzo</th>
+        <th scope="col">Tipo</th>
+        <th scope="col">Data Uscita</th>
+        <th scope="col">Serie</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr >
+        @foreach ($comics as $comic)
+        <td>{{$comic->id}}</td>
+        <td>{{$comic->title}}</td>
+        <td>{{$comic->price}}</td>
+        <td>{{$comic->type}}</td>
+        <td>{{$comic->sale_date}}</td>
+        <td>{{$comic->series}}</td>
+        <td>
+          <a href="{{route('comic.show', $comic)}}" title="vai" class="btn btn-success my-4"><i class="fa-solid fa-eye"></i></a>
+        </td>
+        <td>
+          <a href="{{route('comic.show', $comic)}}" title="modifica" class="btn btn-primary my-4"><i class="fa-solid fa-pencil"></i></a>
+        </td>
+        <td>
+          <a href="{{route('comic.show', $comic)}}" title="delete" class="btn btn-danger my-4"><i class="fa-solid fa-trash-can"></i></a>
+        </td>
+      </tr>
       @endforeach
-  </div>
-</div>
+    </tbody>
+  </table>
 @endsection
 
 
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -7,15 +7,21 @@
 
       <div class="col my-4" >
           <div class="card px-4 h-100" style="width: 80rem;">
-            <div class="card-body">
-              <h5 class="card-title">Descrizione:</h5>
-              <p class="card-text">{{$comic->description}}</p>
-              <h5 class="card-title">Artisti:</h5>
-              <p>{{$comic->artists}}</p>
-              <h5 class="card-title">Scrittori:</h5>
-              <p>{{$comic->writers}}</p>
+            <h2 class="text-center fw-bold my-5">{{$comic->title}}</h2>
+            <div class="container-img  ">
+              <img src="{{$comic->thumb}}" class="card-img-top  alt="...">
             </div>
-            <a href="{{route('comic.index')}}" class="btn btn-primary my-4">Torna indietro</a>
+            <div class="card-body">
+              <h5 class="card-title fw-bold">Descrizione:</h5>
+              <p class="card-text">{{$comic->description}}</p>
+              <h5 class="card-title fw-bold">Artisti:</h5>
+              <p>{{$comic->artists}}</p>
+              <h5 class="card-title fw-bold">Scrittori:</h5>
+              <p>{{$comic->writers}}</p>
+              <div class="col text-center">
+                <a href="{{route('comic.index')}}" class="btn btn-primary my-4 fw-bold">Torna indietro</a>
+              </div>
+            </div>
           </div>
         </div>
 
